9d75bfd299e0d0c3bf2540713c80c4096d273f72
mrStatic23 <mrstatic23@yandex.ru>
2020-09-29 21:16:22 +0300

    init: start and finish task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,29 @@
 
 exports.min = function min (array) {
+  
+  if (Array.isArray(array) && array.length !== 0)  { 
+    return array.sort((a,b) => b - a).pop();
+  }
+    
   return 0;
 }
 
 exports.max = function max (array) {
+
+  if (Array.isArray(array) && array.length !== 0)  { 
+    return array.sort((a,b) => a - b).pop();
+  }
+
   return 0;
 }
 
 exports.avg = function avg (array) {
+
+
+  if (Array.isArray(array) && array.length !== 0)  { 
+    return (array.reduce((sum, current) => sum + current, 0) / array.length);
+  }
+
   return 0;
+
 }
